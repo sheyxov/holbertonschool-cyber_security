4f82090b6b18b411f556ea28ef132ec65145ba0d
sheyxov <cavansirsheyxov@gmail.com>
2026-01-18 00:20:25 +0400

    Task

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# Decode IBM WebSphere XOR encoded strings
+# Decode IBM WebSphere XOR encoded passwords
 
 hash="$1"
 
-clean_hash="${hash#\{xor\}}"
+clean="${hash#\{xor\}}"
 
-decoded="$(printf "%s" "$clean_hash" | base64 -d)"
-
-key="$(printf "%s" "$decoded" | od -An -t u1 | awk 'NR==1 { print $1 }')"
-
-printf "%s" "$decoded" \
-| od -An -t u1 \
-| awk '
-NR==1 { next }
-{
-    for (i = 1; i <= NF; i++)
-    {
-        printf "%c", ($i ^ key)
-    }
+printf "%s" "$clean" | base64 -d | perl -0777 -ne '
+@b = unpack("C*", $_);
+$k = shift @b;
+for (@b) {
+    print chr($_ ^ $k);
 }
+print "\n";
 '
-printf "\n"
