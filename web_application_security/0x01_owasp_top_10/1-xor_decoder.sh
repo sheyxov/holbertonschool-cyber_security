9cd808684187fbbcb7dfd3321b2fd9dd1e608b06
sheyxov <cavansirsheyxov@gmail.com>
2026-01-18 00:21:06 +0400

    Task

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -6,10 +6,8 @@ hash="$1"
 clean="${hash#\{xor\}}"
 
 printf "%s" "$clean" | base64 -d | perl -0777 -ne '
-@b = unpack("C*", $_);
-$k = shift @b;
-for (@b) {
-    print chr($_ ^ $k);
+for (unpack("C*", $_)) {
+    print chr($_ ^ 0x5f);
 }
 print "\n";
 '
