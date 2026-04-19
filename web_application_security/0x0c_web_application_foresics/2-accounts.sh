b886664204d6e408e503de564cbeb1a8ada1cb6d
sheyxov <cavansirsheyxov@gmail.com>
2026-04-19 14:09:11 -0400

    task

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-tail -n 1000 auth.log | \
+tail -n 1000 $1 | \
 grep "Failed password\|Accepted password" | \
-awk '{for(i=1;i<=NF;i++){if($i=="for"){print $(i+1)}}}' | \
+awk '{
+    for(i=1;i<=NF;i++){
+        if($i=="for"){
+            if($(i+1)=="invalid"){
+                print $(i+3)
+            } else {
+                print $(i+1)
+            }
+        }
+    }
+}' | \
 sort | \
 uniq -c | \
 sort -nr | \
